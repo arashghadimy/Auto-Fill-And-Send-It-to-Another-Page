649d1e929d957061f8f2266154ef7c8318c1b36e
arashghadimy <103946253+arashghadimy@users.noreply.github.com>
2025-11-23 00:11:49 +0330

    Update Secend Page Style

--- app/src/main/java/com/example/auto_fillin_class_poject/ResultActivity.java
+++ app/src/main/java/com/example/auto_fillin_class_poject/ResultActivity.java
@@ -1,5 +1,5 @@
 package com.example.auto_fillin_class_poject;
-import android.os.Bundle;
+/*import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,12 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ResultActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super .onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_main);
+
+
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtFamily = findViewById(R.id.txtFamily);
         TextView txt = new TextView(this);
         txt.setTextSize(24);
         String name = getIntent().getStringExtra("name");
         String family = getIntent().getStringExtra("family");
         txt.setText("name: "+name+ "\nfamily:" +family);
+    }
+}*/
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-        setContentView(txt);
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_main);  // فقط همین!
+
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtFamily = findViewById(R.id.txtFamily);
+
+        String name = getIntent().getStringExtra("name");
+        String family = getIntent().getStringExtra("family");
+
+        txtName.setText("Name: " + name);
+        txtFamily.setText("Family: " + family);
     }
 }
